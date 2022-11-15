d345f15e295d6593097b811c545a8f87de93e7b0
julien-fort <114388429+julien-fort@users.noreply.github.com>
2022-11-15 16:39:56 +0100

    Add files via upload

---
+++ jour02/job04.php
@@ -0,0 +1,18 @@
+<?php
+for( $i = 0 ; $i < 100 ; $i++ ) {
+    
+    if ($i % 3 && $i % 5) {
+            echo 'FizzBuzz <br>';
+    }
+    elseif ($i % 3) {
+        echo 'Fizz <br>';
+    }
+    elseif ($i % 5) {
+        echo 'Buzz <br>';
+    }
+   
+else {
+    echo $i.'<br>';
+    }
+}
+?>
